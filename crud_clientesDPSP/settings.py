861ba713b4a1cf09e771258eea784a234aed6545
diana santiago <Diana291.esmecamp2024@gmail.com>
2024-11-29 12:59:56 -0600

    cambios allowed host

--- crud_clientesDPSP/settings.py
+++ crud_clientesDPSP/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-ka75*m!+60zlt80l1=v*1k18d*lnp#z25teih7trtos&2)u8p!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
